06044c4ea4c405fbd502de550c953295a09664ba
johnkagga <johnkagga@gmail.com>
2018-02-26 15:59:26 +0300

    Add sherlock solution

--- sherlock.js
+++ sherlock.js
@@ -1,12 +1,24 @@
-/**Work in progress
- *
+/**
+ * Sherlock Array solution.
+ * Input should be the same as shown in the question.
  * @param input
  */
 const sherlock = input => {
   const lines = input.split('\n');
   let testCases = parseInt(lines[0]);
   if ((lines.length - 1) / 2 === testCases) {
-
+    for (let i = 1; i < lines.length - 1; i++) {
+      const numOfElements = parseInt(lines[i]);
+      const testCase = lines[i + 1].split(' ').map(num => parseInt(num));
+      if (numOfElements === testCase.length) {
+        const add = (a, b) => a + b;
+        for (let y = 1; y < numOfElements - 1; y++) {
+          if (testCase.slice(0, y).reduce(add) === testCase.slice(y + 1).reduce(add)) console.log("YES");
+          else console.log('NO')
+        }
+      }
+      i += 1;
+    }
   }
 };
 
